612269db0245ff2ad57c11d3b534a64ea3a4c581
Pankaj Chaudhary <pankajchaudharywork@gmail.com>
2024-05-08 17:57:13 -0600

    code cleanup

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,4 @@
-import useData from "./useData";
 import platforms from "../data/platforms";
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatform = () => ({ data: platforms, isLoading: false, error: false });
 
